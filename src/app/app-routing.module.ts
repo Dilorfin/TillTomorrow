aaceba964900116af11705ded4814d2052e12c31
Dilorfin <pavlo_kuibida@epam.com>
2022-04-21 20:11:47 +0300

    Add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-	{ path: '', component: MainPageComponent }
+	{ path: '', component: MainPageComponent },
+	{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
