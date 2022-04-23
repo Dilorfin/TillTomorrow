599e91451f4cf1a9341b3c4d592799b1f0083735
Dilorfin <pavlo_kuibida@epam.com>
2022-04-23 11:14:54 +0300

    Add switch language buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent
 {
-	title = 'till-tomorrow';
+	constructor(@Inject(LOCALE_ID) public locale: string)
+	{}
 }
