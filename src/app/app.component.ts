f9a53e2308306dcb4d743be62c228aaf90c4bb32
Dilorfin <pavlo_kuibida@epam.com>
2022-04-24 17:11:38 +0300

    Migrate less to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, Inject, LOCALE_ID } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.less']
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent
 {
