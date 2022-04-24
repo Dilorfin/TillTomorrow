bc550d14aaaf77df94a5a353bd787883b8bfc0d0
Dilorfin <pavlo_kuibida@epam.com>
2022-04-24 23:56:00 +0300

    Move side menu to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,32 @@ import { Component, Inject, LOCALE_ID } from '@angular/core';
 })
 export class AppComponent
 {
+	public navigationItems:{
+		route:string,
+		title:string
+	}[] = [
+		{
+			route: "",
+			title: $localize `:@@app-title:Till Tomorrow`
+		},
+		{
+			route: "",
+			title: $localize `:@@technics-title:Technics`
+		},
+		{
+			route: "",
+			title: $localize `:@@stories-title:Cool-stories`
+		},
+		{
+			route: "",
+			title: $localize `:@@for-relatives-title:For relatives`
+		},
+		{
+			route: "about-us",
+			title: $localize `:@@about-us-title:About us`
+		}
+	];
+	
 	constructor(@Inject(LOCALE_ID) public locale: string)
 	{}
 }
