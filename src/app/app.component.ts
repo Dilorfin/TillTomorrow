83cf22ccac8fe0654a808cde4c93a9b582ab7d89
Dilorfin <pavlo_kuibida@epam.com>
2022-04-27 23:56:19 +0300

    Change language changing button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,59 @@ import { Component, Inject, LOCALE_ID } from '@angular/core';
 })
 export class AppComponent
 {
-	public navigationItems:{
-		route:string,
-		title:string
+	public navigationItems: {
+		route: string,
+		title: string
 	}[] = [
-		{
-			route: "",
-			title: $localize `:@@app-title:Till Tomorrow`
-		},
-		{
-			route: "articles",
-			title: $localize `:@@technics-title:Technics`
-		},
-		{
-			route: "stories",
-			title: $localize `:@@stories-title:Cool-stories`
-		},
-		{
-			route: "for-relatives",
-			title: $localize `:@@for-relatives-title:For relatives`
-		},
-		{
-			route: "about-us",
-			title: $localize `:@@about-us-title:About us`
-		}
-	];
-	
+			{
+				route: "",
+				title: $localize`:@@app-title:Till Tomorrow`
+			},
+			{
+				route: "articles",
+				title: $localize`:@@technics-title:Technics`
+			},
+			{
+				route: "stories",
+				title: $localize`:@@stories-title:Cool-stories`
+			},
+			{
+				route: "for-relatives",
+				title: $localize`:@@for-relatives-title:For relatives`
+			},
+			{
+				route: "about-us",
+				title: $localize`:@@about-us-title:About us`
+			}
+		];
+	public currentLanguage: string = "";
+	public languagesList: {
+		title: string,
+		code: string,
+		link: string
+	}[] = [
+			{
+				code: "uk",
+				title: "Ukrainian",
+				link: "/"
+			},
+			{
+				code: "en",
+				title: "English",
+				link: "/en"
+			},
+			{
+				code: "ru",
+				title: "Russian",
+				link: "/ru"
+			}
+		];
+
 	constructor(@Inject(LOCALE_ID) public locale: string)
-	{}
+	{
+		this.currentLanguage = this.languagesList.find((value) =>
+		{
+			return value.code == locale;
+		})?.title || "";
+	}
 }
