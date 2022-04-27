b08904056888f280309324efb75dc0b5d7f3ef6f
Dilorfin <pavlo_kuibida@epam.com>
2022-04-28 00:00:22 +0300

    Add edit article page

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EditArticlePageComponent } from './edit-article-page/edit-article-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    EditArticlePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
