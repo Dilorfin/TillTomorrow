3945bd9d466795dad530de392f8a0636d20f1d71
Dilorfin <pavlo_kuibida@epam.com>
2022-05-05 16:44:48 +0300

    Test for auth

--- api/GetArticlesList/index.ts
+++ api/GetArticlesList/index.ts
@@ -1,18 +1,24 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+async function getUserInfo()
+{
+	const response = await fetch('/.auth/me');
+	const payload = await response.json();
+	const { clientPrincipal } = payload;
+	return clientPrincipal;
+}
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void>
 {
-	context.log('HTTP trigger function processed a request.');
-	const name = (req.query.name || (req.body && req.body.name));
-	const responseMessage = name
-		? "Hello, " + name + ". This HTTP triggered function executed successfully."
-		: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+	const user = getUserInfo();
 
+	context.log('HTTP trigger function processed a request.');
+	context.log(user);
 	context.res = {
 		// status: 200, /* Defaults to 200 */
-		body: { text: responseMessage }
+		body: user
 	};
-
+	
 };
 
 export default httpTrigger;
