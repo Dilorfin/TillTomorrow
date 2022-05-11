1775e8431f697d3540474d135e8c2fb69c7979b4
Dilorfin <pavlo_kuibida@epam.com>
2022-05-11 19:46:42 +0300

    Add first try for UpdateArticle

--- api/UpdateArticle/index.ts
+++ api/UpdateArticle/index.ts
@@ -1,18 +1,76 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Collection, Db, MongoClient, ObjectId } from "mongodb";
+import { AzureUserData, getAzureUserFromRequest, setErrorResult } from "../common/utils";
+import { ArticleModel } from "../models/api/article.model";
+import { ArticleDbModel } from "../models/db/article.model";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void>
+const client = new MongoClient(process.env.CONNECTION_STRING);
+
+const httpTrigger: AzureFunction = async function (context: Context, request: HttpRequest): Promise<void>
 {
-	context.log('HTTP trigger function processed a request.');
-	const name = (req.query.name || (req.body && req.body.name));
-	const responseMessage = name
-		? "Hello, " + name + ". This HTTP triggered function executed successfully."
-		: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+	if (!request.body)
+	{
+		setErrorResult(context, 400, "Request body is empty");
+		return;
+	}
 
-	context.res = {
-		// status: 200, /* Defaults to 200 */
-		body: responseMessage
+	const inputArticle: ArticleModel = request.body;
+	if (!inputArticle.id)
+	{
+		setErrorResult(context, 400, "Article id not provided");
+		return;
+	}
+	if (!inputArticle.htmlText && !inputArticle.title && !inputArticle.language)
+	{
+		setErrorResult(context, 400, "No data provided for article update");
+		return;
+	}
+
+	const user: AzureUserData = getAzureUserFromRequest(request);
+	const now: number = Date.now();
+
+	let updateModel = {
+		modifiedDate: now,
+		modifierId: user.userId
 	};
 
+	if (inputArticle.htmlText)
+	{
+		updateModel["htmlText"] = inputArticle.htmlText;
+	}
+	if (inputArticle.title)
+	{
+		updateModel["title"] = inputArticle.title;
+	}
+	if (inputArticle.language)
+	{
+		updateModel["language"] = inputArticle.language;
+	}
+
+	try
+	{
+		await client.connect();
+		const database: Db = client.db('till-tomorrow');
+		const articles: Collection<ArticleDbModel> = database.collection<ArticleDbModel>('articles');
+
+		const result = await articles.updateOne(
+			{_id: new ObjectId(inputArticle.id) },
+			{ $set: updateModel }
+		);
+
+		if (result.matchedCount <= 0)
+		{
+			setErrorResult(context, 404, `No document was found with id: ${inputArticle.id}`);
+		}
+	}
+	catch (error)
+	{
+		setErrorResult(context, 500, `Error occurred "${error}"`);
+	}
+	finally
+	{
+		await client.close();
+	}
 };
 
 export default httpTrigger;
