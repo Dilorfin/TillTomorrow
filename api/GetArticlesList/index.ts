c862cd15f8769056b83ef0985b4ee14607b94cbe
Dilorfin <pavlo_kuibida@epam.com>
2022-05-11 20:59:49 +0300

    Add GetArticlesList first implementation

--- api/GetArticlesList/index.ts
+++ api/GetArticlesList/index.ts
@@ -1,12 +1,34 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { ArticleDbModel } from '../models/db/article.model';
+import { ArticleModel } from '../models/api/article.model';
+import { FindCursor, MongoClient, WithId } from "mongodb";
+import { mapper } from "../models/mapper";
+import { setErrorResult } from "../common/utils";
 
-import { getAzureUserFromRequest, AzureUserData } from "../common/utils"
+const client = new MongoClient(process.env.CONNECTION_STRING);
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void>
+const httpTrigger: AzureFunction = async function (context: Context, request: HttpRequest): Promise<void>
 {
-	const user : AzureUserData = getAzureUserFromRequest(req);
+	try
+	{
+		await client.connect();
+		const database = client.db('till-tomorrow');
+		const articlesCollection = database.collection<ArticleDbModel>('articles');
+		
+		const cursorDbArticle: FindCursor<WithId<ArticleDbModel>> = await articlesCollection.find();
 
-	context.res.json(user);
+		const articles = await cursorDbArticle.toArray();
+		const apiArray = mapper.mapArray(articles, ArticleDbModel, ArticleModel);
+		context.res.json(apiArray);
+	}
+	catch (error)
+	{
+		setErrorResult(context, 500, `Error occurred "${error}"`);
+	}
+	finally
+	{
+		await client.close();
+	}
 };
 
 export default httpTrigger;
